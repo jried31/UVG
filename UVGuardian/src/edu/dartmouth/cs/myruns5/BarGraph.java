f6f0665047ebabc61e58ab1fbe9445e8c258ec73
NathanCT <nathan.tung@sbcglobal.net>
2013-12-05 20:12:54 -0800

    improve bar graph, update friends list fragment

--- UVGuardian/src/edu/dartmouth/cs/myruns5/BarGraph.java
+++ UVGuardian/src/edu/dartmouth/cs/myruns5/BarGraph.java
@@ -6,14 +6,16 @@ import org.achartengine.ChartFactory;
 import org.achartengine.chart.BarChart.Type;
 import org.achartengine.model.CategorySeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
-
-import com.parse.ParseUser;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint.Align;
+
+import com.parse.ParseUser;
 
 public class BarGraph{
 
@@ -21,93 +23,109 @@ public class BarGraph{
 	private ArrayList<ParseUser> fbFriends; // Stores all Facebook Friends that user is Friends with
 	
 	//there should be a color array here with 5 different colors
-		// that specify 5 different people (user + 4 friends)
+	// that specify 5 different people (user + 4 friends)
 	
-	public BarGraph(ArrayList<Integer> selectedItems, ArrayList<ParseUser> fbFriends){
-		this.selectedItems = selectedItems;
-	 this.fbFriends = fbFriends;
+	public BarGraph(ArrayList<Integer> selectedItemsIn, ArrayList<ParseUser> fbFriendsIn) {
+		selectedItems = selectedItemsIn;
+		fbFriends = fbFriendsIn;
+		//selectedItems and fbFriends might have NullPointerException here
+		//System.out.println(selectedItems.size());
+		//System.out.println(fbFriends.size());
+		
 	}
 	
-	public Intent getIntent(Context context) 
-	{	
-
-		int[] y1 = { 0,111 };
-		CategorySeries series1 = new CategorySeries("Demo Arun");
-		for (int i = 0; i < y1.length; i++) {
-			series1.add("Bar " + (i+1), y1[i]);
-		}
-			
-		int[] y2 = { 0,222 };
-		CategorySeries series2 = new CategorySeries("Demo Nathan");
-		for (int j = 0; j < y2.length; j++) {
-			series2.add("Bar " + (j+1), y2[j]);
-		}
-			
-		int[] y3 = { 0,50 };
-		CategorySeries series3 = new CategorySeries("Demo Julian");
-		for (int k = 0; k < y3.length; k++) {
-			series3.add("Bar " + (k+1), y3[k]);
-		}
-		
-		int[] y4 = { 0,150 };
-		CategorySeries series4 = new CategorySeries("Demo Matt");
-		for (int m = 0; m < y4.length; m++) {
-			series4.add("Bar " + (m+1), y4[m]);
-		}
-		
-		
+	public Intent getIntent(Context context) {	
 		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-		dataset.addSeries(series1.toXYSeries());
-		dataset.addSeries(series2.toXYSeries());
-		dataset.addSeries(series3.toXYSeries());
-		dataset.addSeries(series4.toXYSeries());
-		// This is how the "Graph" itself will look like
-		XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
-		mRenderer.setChartTitle("Relative UV Exposure");
-		//mRenderer.setXTitle("X VALUES");
-		mRenderer.setXTitle("Facebook Friends");
-		//mRenderer.setYTitle("Y VALUES");
-		mRenderer.setYTitle("UV exposure");
-		mRenderer.setAxesColor(Color.GREEN);
-	    mRenderer.setLabelsColor(Color.RED);
-	   
+		
+		//Initialize different series for user/friends
+		XYSeries you = new XYSeries("You");
+        XYSeries friend2 = new XYSeries("Friend2");
+        XYSeries friend3 = new XYSeries("Friend3");
+        XYSeries friend4 = new XYSeries("Friend4");
+        XYSeries friend5 = new XYSeries("Friend5");
+        
+        //Set the UV data points
+        you.add(1,10);
+        friend2.add(2,35);
+        friend3.add(3,50);
+        friend4.add(4,20);
+        friend5.add(5,7);
+        
+        //Add series to dataset
+        dataset.addSeries(you);
+        dataset.addSeries(friend2);
+        dataset.addSeries(friend3);
+        dataset.addSeries(friend4);
+        dataset.addSeries(friend5);
+		
+        //Set up the chart design
+        XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+        mRenderer.setChartTitle("Relative UV Exposure");
+ 		mRenderer.setXTitle("Facebook Friends");
+ 		mRenderer.setYTitle("UV Exposure");
+ 		mRenderer.setAxesColor(Color.BLACK);
+ 		mRenderer.setLabelsColor(Color.RED);
+ 	    mRenderer.setApplyBackgroundColor(true);
+ 	    mRenderer.setBackgroundColor(Color.WHITE);
+ 	    mRenderer.setMarginsColor(Color.WHITE);
+ 	    mRenderer.setBarSpacing(-0.5);
+       	mRenderer.setMargins(new int[] {20, 30, 15, 0});
+       	mRenderer.setShowLegend(false);
+       	mRenderer.setPanEnabled(false);
+       	mRenderer.setZoomEnabled(false, false);
+       	mRenderer.setAxisTitleTextSize(24);
+       	mRenderer.setChartTitleTextSize(32);
+       	mRenderer.setLabelsTextSize(20);
+       	mRenderer.setLegendTextSize(16);
+       	mRenderer.setBarWidth(50);
+       	mRenderer.setXAxisMin(-1.5);
+       	mRenderer.setXAxisMax(7.5);
+       	mRenderer.setYAxisMin(0);
+       	mRenderer.setYAxisMax(200);
+       	mRenderer.setYLabelsAlign(Align.RIGHT);
+       	mRenderer.setXLabelsColor(Color.BLACK);
+       	mRenderer.setYLabelsColor(0, Color.BLACK);
+       	mRenderer.setXLabels(0);
+       	mRenderer.addXTextLabel(-0.5, "You");
+       	mRenderer.addXTextLabel(1.25, "Demo2");
+       	mRenderer.addXTextLabel(3, "Demo3");
+       	mRenderer.addXTextLabel(4.75, "Demo4");
+       	mRenderer.addXTextLabel(6.5, "Demo5");
 	    
-	    
-	    //Add each person selected
-	    // **** notes ****
-	    //  Colors should not be manually selected as so,
-	    // but instead by using the array to be intitialized in 
-	    // the class members
-	    
-	    
-	    // Customize bar 1
+	    //Customize bar 1
 		XYSeriesRenderer renderer = new XYSeriesRenderer();
-		renderer.setColor(Color.YELLOW);
+		renderer.setColor(Color.parseColor("#911000")); //deep red
 		renderer.setDisplayChartValues(true);
-	    renderer.setChartValuesSpacing((float) 0.25);
+	    renderer.setChartValuesTextSize(24);
 	    mRenderer.addSeriesRenderer(renderer);
 	  
-	    
-	    // Customize bar 2
+	    //Customize bar 2
 	    XYSeriesRenderer renderer2 = new XYSeriesRenderer();
-	    renderer2.setColor(Color.CYAN);
+	    renderer2.setColor(Color.parseColor("#003399")); //dark blue
 	    renderer2.setDisplayChartValues(true);
-	    renderer2.setChartValuesSpacing((float) 0.25);
+	    renderer2.setChartValuesTextSize(24);
 	    mRenderer.addSeriesRenderer(renderer2);
 
-	 // Customize bar 3
+	    //Customize bar 3
 	    XYSeriesRenderer renderer3 = new XYSeriesRenderer();
-	    renderer3.setColor(Color.RED);
+	    renderer3.setColor(Color.parseColor("#006600")); //dark green
 	    renderer3.setDisplayChartValues(true);
-	    renderer3.setChartValuesSpacing((float) 0.25);
+	    renderer3.setChartValuesTextSize(24);
 	    mRenderer.addSeriesRenderer(renderer3);
 
-	 // Customize bar 4
+	    //Customize bar 4
 	    XYSeriesRenderer renderer4 = new XYSeriesRenderer();
-	    renderer4.setColor(Color.GREEN);
+	    renderer4.setColor(Color.parseColor("#CC00CC")); //magenta
 	    renderer4.setDisplayChartValues(true);
-	    renderer4.setChartValuesSpacing((float) 0.25);
+	    renderer4.setChartValuesTextSize(24);
 	    mRenderer.addSeriesRenderer(renderer4);
+
+		//Customize bar 5
+	    XYSeriesRenderer renderer5 = new XYSeriesRenderer();
+	    renderer5.setColor(Color.parseColor("#663300")); //brown
+	    renderer5.setDisplayChartValues(true);
+	    renderer5.setChartValuesTextSize(24);
+	    mRenderer.addSeriesRenderer(renderer5);
 	    
 		Intent intent = ChartFactory.getBarChartIntent(context, dataset,mRenderer, Type.DEFAULT);
 		return intent;
